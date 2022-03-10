989a81a54dff4f70b14d2d34db7eda3e7e2a8f43
Pranav Pavithran Sheena <1288@trenser.com>
2022-03-10 10:41:45 +0530

    screen shot capture functionality completed

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+	const captureButton = document.querySelector("#capture-button");
+	const sceneEl = document.querySelector('a-scene');
+
+	const capture = (renderer, scene, camera) => {
+
+		const video = document.querySelector('video')
+		const renderCanvas = renderer.domElement;
+	
+		// output canvas
+		const canvas = document.createElement('canvas');
+		const context = canvas.getContext('2d');
+		canvas.width = renderCanvas.width;
+		canvas.height = renderCanvas.height;
+	
+		const sx = (video.clientWidth - renderCanvas.clientWidth) / 2 * video.videoWidth / video.clientWidth;
+		const sy = (video.clientHeight - renderCanvas.clientHeight) / 2 * video.videoHeight / video.clientHeight;
+		const sw = video.videoWidth - sx * 2;
+		const sh = video.videoHeight - sy * 2;
+	
+		context.drawImage(video, sx, sy, sw, sh, 0, 0, canvas.width, canvas.height);
+	
+		renderer.preserveDrawingBuffer = true;
+		renderer.render(scene, camera); // empty if not run
+		context.drawImage(renderCanvas, 0, 0, canvas.width, canvas.height);
+		renderer.preserveDrawingBuffer = false;
+	
+		const data = canvas.toDataURL('image/png');
+	
+		const link = document.createElement('a');
+		link.download = 'ar_screnshot.png';
+		link.href = data;
+		link.click();
+	}
+	
+	captureButton.addEventListener('click', () => {
+		const renderer = sceneEl.renderer;
+		const camera = sceneEl.camera;
+		const scene = sceneEl.sceneEl.object3D;
+	
+		capture(renderer, scene, camera);
+	});
+} )
 
 function loadMarkers() {
 
