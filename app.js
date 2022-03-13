d0d23e2cf8762285d934949b0241f80e45a11082
Pranav Pavithran Sheena <1288@trenser.com>
2022-03-13 13:06:13 +0530

    web share screen shot

--- app.js
+++ app.js
@@ -7,41 +7,87 @@ document.addEventListener("DOMContentLoaded", function () {
 
 		const video = document.querySelector('video')
 		const renderCanvas = renderer.domElement;
-	
+
 		// output canvas
 		const canvas = document.createElement('canvas');
 		const context = canvas.getContext('2d');
 		canvas.width = renderCanvas.width;
 		canvas.height = renderCanvas.height;
-	
+
 		const sx = (video.clientWidth - renderCanvas.clientWidth) / 2 * video.videoWidth / video.clientWidth;
 		const sy = (video.clientHeight - renderCanvas.clientHeight) / 2 * video.videoHeight / video.clientHeight;
 		const sw = video.videoWidth - sx * 2;
 		const sh = video.videoHeight - sy * 2;
-	
+
 		context.drawImage(video, sx, sy, sw, sh, 0, 0, canvas.width, canvas.height);
-	
+
 		renderer.preserveDrawingBuffer = true;
 		renderer.render(scene, camera); // empty if not run
 		context.drawImage(renderCanvas, 0, 0, canvas.width, canvas.height);
 		renderer.preserveDrawingBuffer = false;
-	
+
 		const data = canvas.toDataURL('image/png');
-	
-		const link = document.createElement('a');
-		link.download = 'ar_screnshot.png';
-		link.href = data;
-		link.click();
+		return data;
+
+		// const link = document.createElement('a');
+		// link.download = 'ar_screnshot.png';
+		// link.href = data;
+		// link.click();
 	}
-	
-	captureButton.addEventListener('click', () => {
+
+	// captureButton.addEventListener('click', () => {
+	// 	const renderer = sceneEl.renderer;
+	// 	const camera = sceneEl.camera;
+	// 	const scene = sceneEl.sceneEl.object3D;
+
+	// 	capture(renderer, scene, camera);
+	// });
+
+	const previewImage = document.querySelector("#preview-image");
+	const previewClose = document.querySelector("#preview-close");
+	const preview = document.querySelector("#preview");
+	const previewShare = document.querySelector("#preview-share");
+
+	document.querySelector("#capture").addEventListener("click", () => {
+
 		const renderer = sceneEl.renderer;
 		const camera = sceneEl.camera;
 		const scene = sceneEl.sceneEl.object3D;
-	
-		capture(renderer, scene, camera);
+
+		const data = capture(renderer, scene, camera);
+		preview.style.visibility = "visible";
+		previewImage.src = data;
 	});
-} )
+
+	previewClose.addEventListener("click", () => {
+		preview.style.visibility = "hidden";
+	});
+
+	previewShare.addEventListener("click", () => {
+		const canvas = document.createElement('canvas');
+		canvas.width = previewImage.width;
+		canvas.height = previewImage.height;
+		const context = canvas.getContext('2d');
+		context.drawImage(previewImage, 0, 0, canvas.width, canvas.height);
+
+		canvas.toBlob((blob) => {
+			const file = new File([blob], "photo.png", { type: "image/png" });
+			const files = [file];
+			if (navigator.canShare && navigator.canShare({ files })) {
+				navigator.share({
+					files: files,
+					title: 'AR Photo',
+				})
+			} else {
+				const link = document.createElement('a');
+				link.download = 'photo.png';
+				link.href = previewImage.src;
+				link.click();
+			}
+		});
+	});
+
+})
 
 function loadMarkers() {
 
