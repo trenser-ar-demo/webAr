6710cdc549328ea12a18d32e6a064ed9ded5015a
Pranav Pavithran Sheena <1288@trenser.com>
2022-03-15 09:51:13 +0530

    video recording

--- app.js
+++ app.js
@@ -87,7 +87,71 @@ document.addEventListener("DOMContentLoaded", function () {
 		});
 	});
 
+	const VideoCapture = (renderer, scene, camera) => {
+
+		const video = document.querySelector('video')
+		const renderCanvas = renderer.domElement;
+
+		// output canvas
+		const canvas = document.createElement('canvas');
+		const context = canvas.getContext('2d');
+		canvas.width = renderCanvas.width;
+		canvas.height = renderCanvas.height;
+
+		const sx = (video.clientWidth - renderCanvas.clientWidth) / 2 * video.videoWidth / video.clientWidth;
+		const sy = (video.clientHeight - renderCanvas.clientHeight) / 2 * video.videoHeight / video.clientHeight;
+		const sw = video.videoWidth - sx * 2;
+		const sh = video.videoHeight - sy * 2;
+
+		context.drawImage(video, sx, sy, sw, sh, 0, 0, canvas.width, canvas.height);
+
+		renderer.preserveDrawingBuffer = true;
+		renderer.render(scene, camera); // empty if not run
+		context.drawImage(renderCanvas, 0, 0, canvas.width, canvas.height);
+		renderer.preserveDrawingBuffer = false;
+
+		var videoStream = canvas.captureStream(30);
+		var mediaRecorder = new MediaRecorder(videoStream);
+
+		var chunks = [];
+		mediaRecorder.ondataavailable = function (e) {
+			chunks.push(e.data);
+		};
+
+		mediaRecorder.onstop = function (e) {
+			var blob = new Blob(chunks, { 'type': 'video/mp4' });
+			chunks = [];
+			var videoURL = URL.createObjectURL(blob);
+			var a = document.createElement("a");
+			document.body.appendChild(a);
+			a.style = "display: none";
+			a.href = videoURL;
+			a.download = "fileas";
+			a.click();
+			window.URL.revokeObjectURL(url);
+		};
+		mediaRecorder.ondataavailable = function (e) {
+			chunks.push(e.data);
+		};
+
+		mediaRecorder.start();
+		//setInterval(draw, 300);
+		setTimeout(function () { mediaRecorder.stop(); }, 5000);
+
+	}
+	document.querySelector("#record-button").addEventListener("click", () => {
+		const renderer = sceneEl.renderer;
+		const camera = sceneEl.camera;
+		const scene = sceneEl.sceneEl.object3D;
+
+		VideoCapture(renderer, scene, camera);
+
+	});
+
+
+
 })
+
 
 function loadMarkers() {
 
